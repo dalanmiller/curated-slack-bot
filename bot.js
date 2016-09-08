ca7a836970031e0c4b9764c3fe7e34ead620be43
Daniel Alan Miller <dalan.miller@gmail.com>
2016-09-08 11:51:13 -0700

    init

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Slapp = require('slapp')
+const ConvoStore = require('slapp-convo-beepboop')
+const BeepBoopContext = require('slapp-context-beepboop')
+const
+
+const slapp = Slapp({
+  verify_token: process.env.SLACK_VERIFY_TOKEN,
+  // convo_store: ConvoStore(),
+  context: BeepBoopContext(),
+  log: true,
+  colors: true
+})
+
+slapp.command('/curated', 'add (.*)', (msg, text, url) => {
+
+  msg.respond(`Thanks for adding! ${url}!`)
+})
