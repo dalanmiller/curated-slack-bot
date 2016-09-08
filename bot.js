2ffe9460092f461cf40242476d0305172eadb165
Daniel Alan Miller <dalan.miller@gmail.com>
2016-09-08 12:07:01 -0700

    moar

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
-const
+
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
 
 const slapp = Slapp({
   verify_token: process.env.SLACK_VERIFY_TOKEN,
